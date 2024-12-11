e7f9c084304b864f5a7a40dd2a16ffa264f5b099
vijeyender <maxbussinesssolutions@gmail.com>
2024-12-11 16:03:20 +0530

    porfolio changes

--- src/Pages/Portfolio/Portfolio.page.jsx
+++ src/Pages/Portfolio/Portfolio.page.jsx
@@ -21,8 +21,8 @@ export default function Portfolio(){
                     </div><img className="img-fluid" src={image1} />
                 </a>
                 <div className="portfolio-caption">
-                    <h4>Threads</h4>
-                    <p className="text-muted">Illustration</p>
+                    <h4>Mineral Water Supply Chennai</h4>
+                    <p className="text-muted">Water</p>
                 </div>
             </div>
             <div className="col-sm-6 col-md-4 portfolio-item"><a className="portfolio-link" href="#portfolioModal2" data-bs-toggle="modal">
@@ -31,8 +31,8 @@ export default function Portfolio(){
                     </div><img className="img-fluid" src={image2} />
                 </a>
                 <div className="portfolio-caption">
-                    <h4>Explore</h4>
-                    <p className="text-muted">Graphic Design</p>
+                    <h4>Max Crackers World Chennai</h4>
+                    <p className="text-muted">Crackers</p>
                 </div>
             </div>
             <div className="col-sm-6 col-md-4 portfolio-item"><a className="portfolio-link" href="#portfolioModal3" data-bs-toggle="modal">
@@ -41,8 +41,8 @@ export default function Portfolio(){
                     </div><img className="img-fluid" src={image3} />
                 </a>
                 <div className="portfolio-caption">
-                    <h4>Finish</h4>
-                    <p className="text-muted">Identity</p>
+                    <h4>Max Finance Limited Chennai</h4>
+                    <p className="text-muted">Finance</p>
                 </div>
             </div>
             <div className="col-sm-6 col-md-4 portfolio-item"><a className="portfolio-link" href="#portfolioModal4" data-bs-toggle="modal">
@@ -51,8 +51,8 @@ export default function Portfolio(){
                     </div><img className="img-fluid" src={image4} />
                 </a>
                 <div className="portfolio-caption">
-                    <h4>Lines</h4>
-                    <p className="text-muted">Branding</p>
+                    <h4>Max Super Market Chennai</h4>
+                    <p className="text-muted">Shopping</p>
                 </div>
             </div>
             <div className="col-sm-6 col-md-4 portfolio-item"><a className="portfolio-link" href="#portfolioModal5" data-bs-toggle="modal">
@@ -61,8 +61,8 @@ export default function Portfolio(){
                     </div><img className="img-fluid" src={image5} />
                 </a>
                 <div className="portfolio-caption">
-                    <h4>Southwest</h4>
-                    <p className="text-muted">Website Design</p>
+                    <h4>Max IT Career Guidance Center Chennai</h4>
+                    <p className="text-muted">IT Consultation</p>
                 </div>
             </div>
             <div className="col-sm-6 col-md-4 portfolio-item"><a className="portfolio-link" href="#portfolioModal6" data-bs-toggle="modal">
@@ -71,8 +71,8 @@ export default function Portfolio(){
                     </div><img className="img-fluid" src={image6} />
                 </a>
                 <div className="portfolio-caption">
-                    <h4>Window</h4>
-                    <p className="text-muted">Photography</p>
+                    <h4>Max Sports League Chennai</h4>
+                    <p className="text-muted">Sports</p>
                 </div>
             </div>
         </div>
