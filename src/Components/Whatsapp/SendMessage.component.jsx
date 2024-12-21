9978fd8d1584bb039e35bb52f5fd8e48a10718d6
vijeyender <maxbussinesssolutions@gmail.com>
2024-12-21 11:13:24 +0530

    with token

--- src/Components/Whatsapp/SendMessage.component.jsx
+++ src/Components/Whatsapp/SendMessage.component.jsx
@@ -21,7 +21,7 @@ const sendMessage = async (props) => {
     };
     const config = {
         headers: {
-            'Authorization': 'Bearer '+token,
+            'Authorization': 'Bearer EAAOBJEGgsqIBO3SMWgdX81yfDqiaHE5gqjxhpI5Jwfi6TjmhedDwDeXwpu52ZCNAfFWPZCHiNOs94Tnk9sZAfPDBXdwJLAbPeZAH1lo36rw5GHz0yACYiU4ulMPoywTopZAretnNpVP1u2I9hAqqQQRw8uZBZBnx4FaLt78vqj8S261x2PQW2CZCDmSWbRfSmnJcWAZDZD',
             'Content-Type': 'application/json'
         }
     };
